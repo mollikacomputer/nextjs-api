b8c9d6f68e3bac9c32e13ad66a5069266681ad17
MollikaComputer Provider <mollikacomputer3@gmail.com>
2023-04-21 00:33:56 +0600

    add delete function successfully

--- pages/comments/index.js
+++ pages/comments/index.js
@@ -20,17 +20,30 @@ const CommentsPage = () => {
         const data = await response.json()
         console.log(data);
     }
+    const deleteComment = async (commentId) =>{
+        const response = await fetch(`/api/comments/${commentId}`, {
+            method:'DELETE'
+        })
+        const data = await response.json();
+        console.log(data);
+        fetchComments()
+    }
     return (
         <>
         <input type="text" value={comment} onChange={ e => setComment(e.target.value) } />
 
         <button onClick={submitComment} > Submit Comment</button>
+        <br/>
+        <br/>
             <button onClick={fetchComments}> Load Comments</button>
+        <br/>
+        <br/>
             {
                 comments.map(comment => {
                     return(
                         <div key={comment.id} >
                             {comment.id} {comment.text}
+                            <button onClick={()=>deleteComment(comment.id)}> Delete</button>
                         </div>
                     )
                 })
